c82a44158a94cb57cb11064332ff379d00fd9c45
Santiago Peña Calero <santiagoxpc@gmail.com>
2024-08-05 22:32:23 -0500

    Add gitignore and fix contact

--- components/Contact.js
+++ components/Contact.js
@@ -66,34 +66,6 @@ const Contact = () => {
               </a>
             </p>
           </div>
-          <div className="bg-black p-6 rounded-lg shadow-md text-white">
-            <div className="flex items-center mb-4">
-              <Image
-                src="/icon.png"
-                alt="Kangoo Logo"
-                className="mr-4"
-                width={116 / 2}
-                height={72 / 2}
-              />
-              <h3 className="text-xl font-semibold">Colombia</h3>
-            </div>
-            <p>Tel. +(57) 301 410 6251</p>
-            <p>Colombia</p>
-          </div>
-          <div className="bg-black p-6 rounded-lg shadow-md text-white">
-            <div className="flex items-center mb-4">
-              <Image
-                src="/icon.png"
-                alt="Kangoo Logo"
-                className="mr-4"
-                width={116 / 2}
-                height={72 / 2}
-              />
-              <h3 className="text-xl font-semibold">México</h3>
-            </div>
-            <p>Tel. +(52) 1 477 108 3344</p>
-            <p>México</p>
-          </div>
         </div>
       </div>
     </section>
